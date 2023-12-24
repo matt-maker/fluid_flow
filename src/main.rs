6773b2dcffabb0b544f1ba5808ab011390ee9302
matt-maker <68969720+matt-maker@users.noreply.github.com>
2023-12-24 12:01:37 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
+}
