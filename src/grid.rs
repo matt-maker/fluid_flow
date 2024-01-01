2ab6beac703756dd62d3855ef733bcde9dc75f82
matt-maker <68969720+matt-maker@users.noreply.github.com>
2024-01-01 18:26:22 +0000

    working on integration step

--- src/grid.rs
+++ src/grid.rs
@@ -3,7 +3,7 @@ use bevy::sprite::MaterialMesh2dBundle;
 
 pub const GRID_CELL_SIZE: Vec2 = Vec2::new(8.0, 8.0);
 pub const GRID_WIDTH: i32 = 150;
-pub const GRID_HEIGHT: i32 = 80;
+pub const GRID_HEIGHT: i32 = 100;
 
 #[derive(Component, Debug)]
 pub struct Grid;
@@ -17,6 +17,107 @@ impl GridValues {
     pub fn new(grid_values_vec: Vec<f32>) -> Self {
         Self { grid_values_vec }
     }
+}
+
+#[derive(Component, Debug)]
+pub struct GridU {
+    pub grid_u_vec: Vec<f32>,
+}
+
+impl GridU {
+    pub fn new(grid_u_vec: Vec<f32>) -> Self {
+        Self { grid_u_vec }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct GridV {
+    pub grid_v_vec: Vec<f32>,
+}
+
+impl GridV {
+    pub fn new(grid_v_vec: Vec<f32>) -> Self {
+        Self { grid_v_vec }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct GridnewU {
+    pub grid_newu_vec: Vec<f32>,
+}
+
+impl GridnewU {
+    pub fn new(grid_newu_vec: Vec<f32>) -> Self {
+        Self { grid_newu_vec }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct GridnewV {
+    pub grid_newv_vec: Vec<f32>,
+}
+
+impl GridnewV {
+    pub fn new(grid_newv_vec: Vec<f32>) -> Self {
+        Self { grid_newv_vec }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct GridP {
+    pub grid_p_vec: Vec<f32>,
+}
+
+impl GridP {
+    pub fn new(grid_p_vec: Vec<f32>) -> Self {
+        Self { grid_p_vec }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct GridS {
+    pub grid_s_vec: Vec<f32>,
+}
+
+impl GridS {
+    pub fn new(grid_s_vec: Vec<f32>) -> Self {
+        Self { grid_s_vec }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct GridM {
+    pub grid_m_vec: Vec<f32>,
+}
+
+impl GridM {
+    pub fn new(grid_m_vec: Vec<f32>) -> Self {
+        Self { grid_m_vec }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct GridnewM {
+    pub grid_newm_vec: Vec<f32>,
+}
+
+impl GridnewM {
+    pub fn new(grid_newm_vec: Vec<f32>) -> Self {
+        Self { grid_newm_vec }
+    }
+}
+
+#[derive(Bundle)]
+pub struct GridBundle {
+    pub grid_values: GridValues,
+    pub grid_u: GridU,
+    pub grid_v: GridV,
+    pub grid_newu: GridnewU,
+    pub grid_newv: GridnewV,
+    pub grid_p: GridP,
+    pub grid_s: GridS,
+    pub grid_m: GridM,
+    pub grid_newm: GridnewM,
 }
 
 #[derive(Component, Debug)]
@@ -67,25 +168,36 @@ fn update_cells(
     query_gridvalues: Query<&GridValues, With<Grid>>,
     mut color_material: ResMut<Assets<ColorMaterial>>,
 ) {
-    let grid_vec = query_gridvalues
-        .get_single()
-        .expect("Could not get grid values vector in update_cells in grid.rs");
+    if let Ok(grid_vec) = query_gridvalues.get_single() {
+        let mut counter: usize = 0;
 
-    let mut counter: usize = 0;
-
-    for query_color_material in &query_simcells {
-        if let Some(material) = color_material.get_mut(query_color_material) {
-            material.color = Color::rgba(
-                grid_vec.grid_values_vec[counter],
-                grid_vec.grid_values_vec[counter + 1],
-                grid_vec.grid_values_vec[counter + 2],
-                grid_vec.grid_values_vec[counter + 3],
-            );
-            counter += 4;
+        for query_color_material in &query_simcells {
+            if let Some(material) = color_material.get_mut(query_color_material) {
+                material.color = Color::rgba(
+                    grid_vec.grid_values_vec[counter],
+                    grid_vec.grid_values_vec[counter + 1],
+                    grid_vec.grid_values_vec[counter + 2],
+                    grid_vec.grid_values_vec[counter + 3],
+                );
+                counter += 4;
+            }
         }
     }
 }
 
 fn spawn_grid(mut commands: Commands) {
-    commands.spawn((GridValues::new(Vec::new()), Grid));
+    commands.spawn((
+        GridBundle {
+            grid_values: GridValues::new(Vec::new()),
+            grid_m: GridM::new(Vec::new()),
+            grid_newm: GridnewM::new(Vec::new()),
+            grid_newu: GridnewU::new(Vec::new()),
+            grid_newv: GridnewV::new(Vec::new()),
+            grid_p: GridP::new(Vec::new()),
+            grid_s: GridS::new(Vec::new()),
+            grid_u: GridU::new(Vec::new()),
+            grid_v: GridV::new(Vec::new()),
+        },
+        Grid,
+    ));
 }
--- src/simulate.rs
+++ src/simulate.rs
@@ -1,25 +1,24 @@
-use crate::grid::{Grid, GridValues, GRID_HEIGHT, GRID_WIDTH};
+use crate::grid::{Grid, GridS, GridV, GridValues, GRID_HEIGHT, GRID_WIDTH};
 use bevy::prelude::*;
 
 pub struct SimulatePlugin;
 
 impl Plugin for SimulatePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, simulate);
+        app.add_systems(Update, pop_sim_vec);
         app.add_systems(PostUpdate, clean_up);
     }
 }
 
-fn simulate(mut query: Query<&mut GridValues, With<Grid>>) {
-    let mut grid_vec = query
-        .get_single_mut()
-        .expect("could not find grid_vec in simulate");
-    for i in 0..GRID_WIDTH {
-        for j in 0..GRID_HEIGHT {
-            for _ in 0..3 {
-                grid_vec.grid_values_vec.push(100.0);
+fn pop_sim_vec(mut query: Query<&mut GridValues, With<Grid>>) {
+    if let Ok(mut grid_vec) = query.get_single_mut() {
+        for i in 0..GRID_WIDTH {
+            for j in 0..GRID_HEIGHT {
+                for _ in 0..3 {
+                    grid_vec.grid_values_vec.push(100.0);
+                }
+                grid_vec.grid_values_vec.push((i + j) as f32 / 230.0);
             }
-            grid_vec.grid_values_vec.push((i + j) as f32 / 230.0);
         }
     }
 }
@@ -29,4 +28,25 @@ fn clean_up(mut query: Query<&mut GridValues, With<Grid>>) {
         .get_single_mut()
         .expect("could not find grid_vec in clean up");
     grid_vec.grid_values_vec.clear();
+}
+
+fn integrate(
+    dt: &f32,
+    gravity: &f32,
+    query_grid_s: Query<&GridS, With<Grid>>,
+    mut query_grid_v: Query<&mut GridV, With<Grid>>,
+) {
+    if let Ok(grid_s) = query_grid_s.get_single() {
+        if let Ok(mut grid_v) = query_grid_v.get_single_mut() {
+            for i in 1..GRID_WIDTH {
+                for j in 1..GRID_HEIGHT - 1 {
+                    if grid_s.grid_s_vec[(i * GRID_HEIGHT + j) as usize] != 0.0
+                        && grid_s.grid_s_vec[(i * GRID_HEIGHT + (j - 1)) as usize] != 0.0
+                    {
+                        grid_v.grid_v_vec[(i * GRID_HEIGHT + j) as usize] += gravity * dt;
+                    }
+                }
+            }
+        }
+    }
 }
